2aaafccc43f0cc1b9c9cdd4d48dade6394723e63
Chriskee <chris.kee@hotmail.com>
2022-01-02 23:12:28 +0000

    Change 3.1

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-          Test Git Page 1.3
-          Line 2
-
-          Line 1.3
-=======
-          Test Git Page 2.2
-          Line 2
-
-          Line 2.3
->>>>>>> c80b4f8dcb7f3a7ef6ffabea9e605d83ea084cb4
+          Line 3.1
     </div>
   )
 }
